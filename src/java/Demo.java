d0b1cdceb4c1243b69822ab5e8c238b2a3ff7ed1
hp <hp@DESKTOP-QUM36GE>
2024-01-29 09:40:16 +0530

    Thammudu its my first program on github

---
+++ src/java/Demo.java
@@ -0,0 +1,12 @@
+package java;
+
+public class Demo {
+
+	public static void main(String[] args) {
+	System.setProperty("webdriver.edge.driver","C:\\Users\\hp\\eclipse-workspace\\Selenium\\drivers\\edgedriver_win32\\msedgedriver.exe");
+
+	
+
+	}
+
+}
