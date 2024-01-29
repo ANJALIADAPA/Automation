47a7ab45fd7ae637c35e88d135a27afbe4a52fa4
ANJALIADAPA <157866149+ANJALIADAPA@users.noreply.github.com>
2024-01-29 09:54:03 +0530

    Update Demo.java

--- src/java/Demo.java
+++ src/java/Demo.java
@@ -5,7 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 	System.setProperty("webdriver.edge.driver","C:\\Users\\hp\\eclipse-workspace\\Selenium\\drivers\\edgedriver_win32\\msedgedriver.exe");
 
-	
+	System.out.println("Its my first github program")
 
 	}
 
